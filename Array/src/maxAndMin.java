b59f736d8b20dbf77373b1539170b653c854ae58
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-01 12:45:41 +0530

    optimised solution

--- Array/src/maxAndMin.java
+++ Array/src/maxAndMin.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 Input: arr[] = {22, 14, 8, 17, 35, 3}
 Output:  Minimum element is: 3
@@ -9,7 +11,8 @@ class MaxMin{
     public int min;
 }
 public class maxAndMin {
-
+    //Time Complexity: O(n)
+    //Auxiliary Space: O(1) as no extra space was needed.
     static  MaxMin  findMaxAndMin(int arr[],int max,int min){
         MaxMin mm= new  MaxMin();
         mm.max=max;
@@ -24,9 +27,22 @@ public class maxAndMin {
         }
         return mm;
     }
+
+    //Time complexity:
+    // O(n log n), where n is the number of elements in the array, as we are using a sorting algorithm.
+    //Auxilary Space: is O(1), as we are not using any extra space.
+    static MaxMin findMaxAndMin2(int[] arr,int n){
+        MaxMin mm=new MaxMin();
+        Arrays.sort(arr);
+        mm.max=arr[n-1];
+        mm.min=arr[0];
+        return mm;
+    }
+
     public static void main(String[] args){
         int arr[]={22,14,8,17,35,3};
         MaxMin ans= findMaxAndMin(arr,arr[0],arr[0]);
+//      MaxMin ans= findMaxAndMin2(arr,arr.length);
         System.out.println(ans.max);
         System.out.println(ans.min);
     }
--- Array/src/reverse.java
+++ Array/src/reverse.java
@@ -1,18 +1,39 @@
 public class reverse {
+    //in place solution ,no extra array requires
+    //time complexity:-o(n)
+    //space complexity:-o(1)
     static  void reverse(int arr[],int start,int end){
            int temp;
-
            while(start<end){
-
                temp=arr[start];
                arr[start]=arr[end];
                arr[end]=temp;
                start++;
                end--;
            }
-
     }
 
+    //Non in-place solution,array needed
+    //time complexity:-o(n)
+    //space complexity:-o(n)
+    static  void reverse2(int arr[],int start,int end){
+        int[] rev_arr=new int[arr.length];
+        for(int i=0;i< arr.length;i++){
+            rev_arr[i]=arr[arr.length-1-i];
+        }
+    }
+
+//Time Complexity: O(n). The recursion goes through each element once, so it’s linear.
+//Auxiliary Space Complexity: O(n) for non in-place, O(log n) for in-place (due to recursion stack).
+    static void reverse_rec(int[] arr, int start, int end) {
+        if(start>=end){
+            return;
+        }
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        reverse_rec(arr,start+1,end-1);
+    }
     static  void  printArr(int arr[],int len){
          for(int i=0;i<len;i++){
              System.out.println(arr[i]);
@@ -21,7 +42,11 @@ public class reverse {
     public static void main(String[] args) {
        int  arr[] = {1, 2, 3};
         printArr(arr, 3);
-       reverse( arr,0,arr.length-1);
-       printArr(arr, arr.length);
+       //reverse( arr,0,arr.length-1);
+       //printArr(arr, arr.length);
+       // reverse2( arr,0,arr.length-1);
+       //printArr(arr, arr.length);
+        reverse_rec( arr,0,arr.length-1);
+        printArr(arr, arr.length);
     }
 }
